4d79487fa66875ab36943d33e8995e1619aceb8d
SunXiaodong <sunxiaod168@hotmail.com>
2018-05-03 15:20:53 +0800

    modify

--- src/ProductFoundation.js
+++ src/ProductFoundation.js
@@ -1,123 +1,123 @@
 const Fields = [{
-    name: 'ID',
-    type: 'number'
-  },
-  {
-    name: 'ZZID',
-    type: 'string'
-  },
-  {
-    name: 'Name',
-    type: 'string'
-  },
-  {
-    name: 'BrandID',
-    type: 'number'
-  },
-  {
-    name: 'CatenaID',
-    type: 'number'
-  },
-  {
-    name: 'SortID',
-    type: 'number'
-  },
-  {
-    name: 'XingHao',
-    type: 'string'
-  },
-  {
-    name: 'GuiGe',
-    type: 'string'
-  },
-  {
-    name: 'ZhuCai',
-    type: 'number'
-  },
-  {
-    name: 'FuCai',
-    type: 'number'
-  },
-  {
-    name: 'JiLiangDanWei',
-    type: 'string'
-  },
-  {
-    name: 'FuZhuJiLiangDanWei',
-    type: 'string'
-  },
-  {
-    name: 'QiYongFuZhuYanSe',
-    type: 'number'
-  },
-  {
-    name: 'QiYongRuanTi',
-    type: 'number'
-  },
-  {
-    name: 'ZuiDiCunLiang',
-    type: 'number'
-  },
-  {
-    name: 'ZuiGaoCunLiang',
-    type: 'number'
-  },
-  {
-    name: 'ZongBaoShu',
-    type: 'number'
-  },
-  {
-    name: 'BaoZhuangTiJi',
-    type: 'number'
-  },
-  {
-    name: 'Weight',
-    type: 'number'
-  },
-  {
-    name: 'BeiZhu',
-    type: 'string'
-  },
-  {
-    name: 'Enable',
-    type: 'number'
-  },
-  {
-    name: 'ImagePath',
-    type: 'string'
-  },
-  {
-    name: 'CreateTime',
-    type: 'string'
-  },
-  {
-    name: 'CreateUser',
-    type: 'number'
-  },
-  {
-    name: 'UpdateUser',
-    type: 'number'
-  },
-  {
-    name: 'UpdateTime',
-    type: 'string'
-  },
-  {
-    name: 'IsDelete',
-    type: 'number'
-  },
-  {
-    name: 'ProductCode',
-    type: 'string'
-  },
-  {
-    name: 'ShortName',
-    type: 'string'
-  }
+  name: 'ID',
+  type: 'number'
+},
+{
+  name: 'ZZID',
+  type: 'string'
+},
+{
+  name: 'Name',
+  type: 'string'
+},
+{
+  name: 'BrandID',
+  type: 'number'
+},
+{
+  name: 'CatenaID',
+  type: 'number'
+},
+{
+  name: 'SortID',
+  type: 'number'
+},
+{
+  name: 'XingHao',
+  type: 'string'
+},
+{
+  name: 'GuiGe',
+  type: 'string'
+},
+{
+  name: 'ZhuCai',
+  type: 'number'
+},
+{
+  name: 'FuCai',
+  type: 'number'
+},
+{
+  name: 'JiLiangDanWei',
+  type: 'string'
+},
+{
+  name: 'FuZhuJiLiangDanWei',
+  type: 'string'
+},
+{
+  name: 'QiYongFuZhuYanSe',
+  type: 'number'
+},
+{
+  name: 'QiYongRuanTi',
+  type: 'number'
+},
+{
+  name: 'ZuiDiCunLiang',
+  type: 'number'
+},
+{
+  name: 'ZuiGaoCunLiang',
+  type: 'number'
+},
+{
+  name: 'ZongBaoShu',
+  type: 'number'
+},
+{
+  name: 'BaoZhuangTiJi',
+  type: 'number'
+},
+{
+  name: 'Weight',
+  type: 'number'
+},
+{
+  name: 'BeiZhu',
+  type: 'string'
+},
+{
+  name: 'Enable',
+  type: 'number'
+},
+{
+  name: 'ImagePath',
+  type: 'string'
+},
+{
+  name: 'CreateTime',
+  type: 'string'
+},
+{
+  name: 'CreateUser',
+  type: 'number'
+},
+{
+  name: 'UpdateUser',
+  type: 'number'
+},
+{
+  name: 'UpdateTime',
+  type: 'string'
+},
+{
+  name: 'IsDelete',
+  type: 'number'
+},
+{
+  name: 'ProductCode',
+  type: 'string'
+},
+{
+  name: 'ShortName',
+  type: 'string'
+}
 ];
 const FieldsMapping = {
-  'ID': 'ID',
-  'ZZID': '组织编号',
+  'ID': null,
+  'ZZID': '组织ID',
   'Name': '物品名称',
   'BrandID': '品牌ID',
   'CatenaID': '系列ID',
@@ -128,14 +128,14 @@ const FieldsMapping = {
   'FuCai': '辅材ID',
   'JiLiangDanWei': '计量单位',
   'FuZhuJiLiangDanWei': '辅助计量单位',
-  'QiYongFuZhuYanSe': '启用辅助颜色',
-  'QiYongRuanTi': '启用软体特性',
+  'QiYongFuZhuYanSe': '启用辅色',
+  'QiYongRuanTi': '启用软体',
   'ZuiDiCunLiang': '最低存量',
   'ZuiGaoCunLiang': '最高存量',
-  'ZongBaoShu': '总包数',
+  'ZongBaoShu': '包装数量',
   'BaoZhuangTiJi': '包装体积',
   'Weight': '重量',
-  'BeiZhu': '备注',
+  'BeiZhu': null,
   'Enable': null,
   'ImagePath': null,
   'CreateTime': null,
@@ -148,7 +148,7 @@ const FieldsMapping = {
 };
 const insertSQL = require('./insertSQL');
 
-var makeInsert = function (data, fileName) {
+var makeInsert = function (data, fileName, startID) {
   var now = (new Date()).toLocaleString("zh-CN", {
     hour12: false
   });
@@ -156,7 +156,13 @@ var makeInsert = function (data, fileName) {
   function nullFieldHandler(field) {
 
     var colValue = null;
-    switch (field.name) {     
+    switch (field.name) {
+      case 'ID':
+        colValue = startID++;
+        break;    
+      case 'BeiZhu':
+        colValue = null;
+        break;
       case 'Enable':
         colValue = 1;
         break;
--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const loadJsonFile = require('load-json-file');
 const updateProductRetailPrice = require('./UpdateProductRetailPrice');
 const installDispatchHeader =require('./InstallDispatchHeader');
 
-// excelToJSON('Excel/ProductFoundation', 600, function (jsonFile) {
+excelToJSON('Excel/ProductFoundation', 600, function (jsonFile) {
 
-//     var fileName = path.basename(jsonFile, '.json');
-//     loadJsonFile(jsonFile).then(data => {
+    var fileName = path.basename(jsonFile, '.json');
+    loadJsonFile(jsonFile).then(data => {
         
-//         product.makeInsert(data, fileName);
-//         productPrice.makeInsert(data, fileName, 372);
-//         productRetailPrice.makeInsert(data, fileName, 'V4100008001');
-//     });
-// });
+        product.makeInsert(data, fileName, 862);
+        // productPrice.makeInsert(data, fileName, 372);
+        // productRetailPrice.makeInsert(data, fileName, 'V4100008001');
+    });
+});
 
 // excelToJSON('Excel/ProductRetailPrice', null, function (jsonFile) {
 
@@ -27,10 +27,10 @@ const installDispatchHeader =require('./InstallDispatchHeader');
 // });
 
 
-excelToJSON('Excel/InstallDispatchHeader', null, function (jsonFile) {
+// excelToJSON('Excel/InstallDispatchHeader', null, function (jsonFile) {
 
-    var fileName = path.basename(jsonFile, '.json');
-    loadJsonFile(jsonFile).then(data => {
-        installDispatchHeader.makeInsert(data, fileName);       
-    });
-});
+//     var fileName = path.basename(jsonFile, '.json');
+//     loadJsonFile(jsonFile).then(data => {
+//         installDispatchHeader.makeInsert(data, fileName);       
+//     });
+// });
